e112d3f3a669c4a5e3c211353a974a2bbc9bf20f
jonagull <jonathangullik@gmail.com>
2023-07-01 22:43:39 +0200

    added rules page

---
+++ frontend/src/routes/rules/+page.svelte
@@ -0,0 +1,90 @@
+<div class="p-4">
+    <p class="mb-4">
+        <strong> In the game of 8-ball pool </strong>, players aim to pocket all
+        the balls in their designated group (solids or stripes) while
+        strategically positioning the cue ball. The player must pocket all their
+        designated balls first before attempting to sink the 8 ball to win the
+        game. However, players must be careful not to pocket the 8 ball
+        prematurely or commit fouls, which can result in penalties. The game
+        requires a combination of skill, strategy, and careful shot selection to
+        outsmart the opponent and secure victory.
+    </p>
+
+    <hr class="mb-4" />
+
+    <p class="mb-4">
+        In 8-ball pool, a legal shot is one that meets certain requirements and
+        does not result in a foul. The specific rules may vary depending on the
+        set of rules being followed, but here are some common guidelines for a
+        legal shot in 8-ball pool:
+    </p>
+
+    <ul class="list-disc pl-8 mb-4">
+        <li>
+            <strong>Cue Ball Contact:</strong> The cue ball must strike one of the
+            balls in the player's designated group (either solids or stripes) first,
+            and a ball from that group must be pocketed or hit a rail after the contact.
+        </li>
+        <li>
+            <strong>Object Ball Contact:</strong> After striking a ball from the
+            player's designated group, the cue ball may also contact and pocket balls
+            from the opposing group (opponent's balls), but it is not required.
+        </li>
+        <li>
+            <strong>Ball Pocketed:</strong> At least one ball must be legally pocketed
+            on the shot. If a player pockets a ball from their designated group,
+            they continue their turn; if a ball from the opponent's group is pocketed,
+            it becomes the opponent's turn.
+        </li>
+        <li>
+            <strong>No Fouls:</strong> The shot must not result in a foul, which
+            can include things like scratching (cue ball being pocketed), failing
+            to hit any ball, hitting the cue ball off the table, or violating other
+            specific rules.
+        </li>
+    </ul>
+
+    <p class="mb-4">
+        It's important to note that different rule sets may have additional
+        variations and requirements for a legal shot, such as whether the 8 ball
+        can be pocketed early or the requirement to call shots. Therefore, it's
+        always recommended to clarify and agree upon the specific rules being
+        followed before playing a game of 8-ball pool.
+    </p>
+
+    <hr class="mb-4" />
+
+    <p class="mb-4">
+        In the game of 8-ball pool,<strong class="eightball">
+            "ball in hand" refers to a situation where a player has the freedom
+            to place the cue ball anywhere on the table before taking their
+            shot.
+        </strong>However, the specific rules regarding where the player can
+        place the cue ball after a "ball in hand" situation may vary depending
+        on the rule set being followed.
+    </p>
+
+    <p class="mb-4">
+        In many rule sets, including popular variations of 8-ball pool such as
+        BCA (Billiard Congress of America) and WPA (World Pool-Billiard
+        Association) rules, when a player has "ball in hand," they can place the
+        cue ball anywhere on the table, as long as it is not touching any other
+        ball. There is typically no requirement for the cue ball to be played
+        from a specific area such as the kitchen (also known as the "cue ball in
+        the kitchen" rule).
+    </p>
+
+    <p class="mb-4">
+        <strong> The kitchen is a term </strong> used in some other cue sports games,
+        such as snooker, to refer to a specific designated area of the table where
+        the cue ball must be played from after certain fouls or other specific situations.
+        However, in the context of 8-ball pool, the concept of the kitchen is not
+        commonly used for positioning the cue ball after a "ball in hand" situation.
+    </p>
+
+    <p>
+        It's always a good idea to clarify and agree upon the specific rules
+        being followed with your opponent before playing a game of 8-ball pool
+        to ensure a fair and enjoyable playing experience.
+    </p>
+</div>
