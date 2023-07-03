1468f4ab864efae68a98016ba2e66211d136d2c8
jonagull <jonathangullik@gmail.com>
2023-07-03 22:10:23 +0200

    removing feed from homepage, updating package versions

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -25,24 +25,28 @@
                 href="/game"
                 class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
             >
-                Play game
-                <svg
-                    class="w-5 h-5 ml-2 -mr-1"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                    ><path
-                        fill-rule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                    /></svg
+                <button
+                    class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
+                    Play game
+                    <svg
+                        class="w-5 h-5 ml-2 -mr-1"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                        ><path
+                            fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                        /></svg
+                    >
+                </button>
             </a>
         </div>
     </div>
 </section>
 
-<Feed />
+<!-- <Feed /> -->
 
 <style>
     * {
@@ -52,17 +56,5 @@
     p {
         font-size: 18px;
         margin-bottom: 30px;
-    }
-
-    .cta-button {
-        display: inline-block;
-        padding: 12px 24px;
-        background-color: #333;
-        color: #fff;
-        text-decoration: none;
-        font-size: 18px;
-        border-radius: 4px;
-        transition: background-color 0.3s ease;
-        cursor: pointer;
     }
 </style>
