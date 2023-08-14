9068348f84e61b2d91661ece515fa7f4b592fd47
jonagull <jonathangullik@gmail.com>
2023-08-14 15:14:55 +0200

    crawl

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 
     kit: {
         prerender: {
-            enabled: false,
+            crawl: false,
         },
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
