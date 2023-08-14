37edbc4dc431a03e479854c818b6c9fa73cf5dce
jonagull <jonathangullik@gmail.com>
2023-08-14 15:16:50 +0200

    handehttperror return

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -9,7 +9,9 @@ const config = {
 
     kit: {
         prerender: {
-            crawl: false,
+            handleHttpError: () => {
+                return;
+            },
         },
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
