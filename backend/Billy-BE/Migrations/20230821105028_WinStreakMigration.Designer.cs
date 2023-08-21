c1927b8f839ee828deb19641464a208b172c54e3
Andreas Vikse <andreas.vikse@outlook.com>
2023-08-21 13:06:37 +0200

    Create logic for win streak

--- backend/Billy-BE/Migrations/20230821105028_WinStreakMigration.Designer.cs
+++ backend/Billy-BE/Migrations/20230821105028_WinStreakMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Billy_BE.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Billy_BE.Migrations
 {
     [DbContext(typeof(BillyContext))]
-    partial class BillyContextModelSnapshot : ModelSnapshot
+    [Migration("20230821105028_WinStreakMigration")]
+    partial class WinStreakMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.8");
---
+++ backend/Billy-BE/Migrations/20230821105028_WinStreakMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Billy_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class WinStreakMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                -- Use SQL logic to populate the win streak data
+                -- This example uses SQLite-compatible syntax
+
+                UPDATE Players
+                SET CurrentWinStreak = 0, LongestWinStreak = 0;
+
+                WITH GameWinners AS (
+                    SELECT
+                        CASE WHEN g.PlayerOneId = p.Id THEN g.PlayerOneId ELSE g.PlayerTwoId END AS WinnerId,
+                        g.TimeOfPlay
+                    FROM
+                        GamesPlayed g
+                        JOIN Players p ON g.WinnerId = p.Id
+                )
+                UPDATE Players
+                SET
+                    CurrentWinStreak = (
+                        SELECT COUNT(*) 
+                        FROM (
+                            SELECT MAX(TimeOfPlay) as MaxTimeOfPlay
+                            FROM GameWinners
+                            WHERE WinnerId = Players.Id
+                            GROUP BY strftime('%Y-%m-%d', TimeOfPlay)
+                        ) AS WinStreaks
+                        WHERE WinStreaks.MaxTimeOfPlay = (SELECT MAX(TimeOfPlay) FROM GameWinners)
+                    ),
+                    LongestWinStreak = (
+                        SELECT MAX(StreakCount) FROM (
+                            SELECT
+                                MAX(TimeOfPlay) AS MaxTimeOfPlay,
+                                COUNT(*) AS StreakCount
+                            FROM GameWinners
+                            WHERE WinnerId = Players.Id
+                            GROUP BY strftime('%Y-%m-%d', TimeOfPlay)
+                        ) AS WinStreaks
+                    )
+                WHERE Id IN (SELECT DISTINCT WinnerId FROM GameWinners);
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
