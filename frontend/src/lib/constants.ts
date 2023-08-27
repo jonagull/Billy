e131d2fcf6662fe78abe0692a6900f822f8e2939
jonagull <jonathangullik@gmail.com>
2023-08-27 22:17:53 +0200

    added logic to set baseurl for dev and prod

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,2 +1,7 @@
-// export const baseUrl = import.meta.env.VITE_API_URL;
-export const baseUrl = "https://www.billypool.no/api";
+import { dev } from "$app/environment";
+
+export let baseUrl = "https://www.billypool.no/api";
+
+if (dev) {
+    baseUrl = import.meta.env.VITE_API_URL;
+}
