2c48d359367e7913864d4814a1c022b1f8373154
jonagull <jonathangullik@gmail.com>
2023-09-01 14:19:50 +0200

    remove prerender stuff

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -8,11 +8,11 @@ const config = {
     preprocess: [vitePreprocess({})],
 
     kit: {
-        prerender: {
-            handleHttpError: () => {
-                return;
-            },
-        },
+        // prerender: {
+        //     handleHttpError: () => {
+        //         return;
+        //     },
+        // },
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
