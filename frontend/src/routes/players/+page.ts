45e6fbaf3a435f5e344a3770d6a4986940dd56a6
jonagull <jonathangullik@gmail.com>
2023-09-04 12:10:21 +0200

    fixed player sorting

--- frontend/src/routes/players/+page.ts
+++ frontend/src/routes/players/+page.ts
@@ -2,9 +2,9 @@ import { fetchPageData } from "$lib/helpers/api";
 import type { PageLoad } from "./$types";
 
 export const load = (async () => {
-    const response: any = await fetchPageData("Players");
+    const response: any[] = await fetchPageData("Players");
 
     return {
-        players: response,
+        players: response.sort((a: any, b: any) => b.rating - a.rating),
     };
 }) satisfies PageLoad;
