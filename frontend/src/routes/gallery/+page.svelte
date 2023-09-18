dbd340cb6e6a96687b467ff9b0714de8b9cfc459
Boien <59290906+jonagull@users.noreply.github.com>
2023-09-18 23:09:16 +0200

    added gallery iframe

---
+++ frontend/src/routes/gallery/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Gallery</h1>
+
+<div class="flex justify">
+    <iframe
+        width="1200"
+        height="600"
+        src="https://www.youtube.com/embed/DHrHi-fd_Aw?si=_VmHooC9jTW0vbLJ"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+    />
+</div>
