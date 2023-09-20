301c045e5e324ef32d6486dcdc54468a59e5999c
jonagull <jonathangullik@gmail.com>
2023-09-20 09:33:33 +0200

    added video gallery, redirect after 1 minute, youtube videos

--- frontend/src/routes/gallery/+page.svelte
+++ frontend/src/routes/gallery/+page.svelte
@@ -1,10 +1,18 @@
-<h1>Gallery</h1>
+<script lang="ts">
+    import { youtubeVideos } from "$lib/data/youtubeVideos";
 
-<div class="flex justify">
+    let randomIndex = Math.floor(Math.random() * youtubeVideos.length);
+</script>
+
+<h1 class="mb-5">Gallery</h1>
+
+<h3 class="flex justify-center">{youtubeVideos[randomIndex].title}</h3>
+
+<div class="flex justify.-center">
     <iframe
         width="1200"
         height="600"
-        src="https://www.youtube.com/embed/DHrHi-fd_Aw?si=_VmHooC9jTW0vbLJ"
+        src={youtubeVideos[randomIndex].link}
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
