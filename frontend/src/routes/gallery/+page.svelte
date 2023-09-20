6ebaf3e820a04c33f89e3e77f55141c26a339be1
jonagull <jonathangullik@gmail.com>
2023-09-20 09:50:10 +0200

    resized video frame

--- frontend/src/routes/gallery/+page.svelte
+++ frontend/src/routes/gallery/+page.svelte
@@ -8,10 +8,10 @@
 
 <h3 class="flex justify-center">{youtubeVideos[randomIndex].title}</h3>
 
-<div class="flex justify.-center">
+<div class="video-wrapper flex justify-center">
     <iframe
-        width="1200"
-        height="600"
+        width="1600"
+        height="900"
         src={youtubeVideos[randomIndex].link}
         title="YouTube video player"
         frameborder="0"
@@ -19,3 +19,9 @@
         allowfullscreen
     />
 </div>
+
+<style>
+    .video-wrapper {
+        margin: 0 -200px;
+    }
+</style>
