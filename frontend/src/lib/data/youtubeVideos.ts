e35bd9950abb5f6f0d48dca40789317f6049fd43
jonagull <jonathangullik@gmail.com>
2023-09-20 18:31:47 +0200

    added loop params to vids

--- frontend/src/lib/data/youtubeVideos.ts
+++ frontend/src/lib/data/youtubeVideos.ts
@@ -6,26 +6,26 @@ interface YoutubeVideo {
 export const youtubeVideos: YoutubeVideo[] = [
     {
         title: "Phillipenes vs Germany VM  xxxx",
-        link: "https://www.youtube.com/embed/DHrHi-fd_Aw?si=_VmHooC9jTW0vbLJ&autoplay=1",
+        link: "https://www.youtube.com/embed/DHrHi-fd_Aw?si=_VmHooC9jTW0vbLJ&autoplay=1&loop=1",
     },
     {
         title: " 1995: FINALS - Efren REYES vs. Earl Strickland ",
-        link: "https://www.youtube.com/embed/-g2pF0f-a3o?si=m-aVc6MEsRkjxCyp&autoplay=1",
+        link: "https://www.youtube.com/embed/-g2pF0f-a3o?si=m-aVc6MEsRkjxCyp&autoplay=1&loop=1",
     },
     {
         title: "How To Play Straight Pool Like Earl Strickland",
-        link: "https://www.youtube.com/embed/UsAoyAeNNQw?si=9TcYKl5_U-EAErg1&autoplay=1",
+        link: "https://www.youtube.com/embed/UsAoyAeNNQw?si=9TcYKl5_U-EAErg1&autoplay=1&loop=1",
     },
     {
         title: "Efren Reyes Greatest Shots in his Career | The Magician",
-        link: "https://www.youtube.com/embed/suoBgkiPchE?si=OSVDNxF_czB1yb14&autoplay=1",
+        link: "https://www.youtube.com/embed/suoBgkiPchE?si=OSVDNxF_czB1yb14&autoplay=1&loop=1",
     },
     {
         title: "30 TOP SHOTS FOREVER With Magician Efren Bata Reyes",
-        link: "https://www.youtube.com/embed/K5EoYhP5Zy4?si=UGkNiSMEipZoh1Uy&autoplay=1",
+        link: "https://www.youtube.com/embed/K5EoYhP5Zy4?si=UGkNiSMEipZoh1Uy&autoplay=1&loop=1",
     },
     {
         title: "EFREN REYES 5 FAMOUS SHOTS THAT SHOCKED THE WORLD OF POOL",
-        link: "https://www.youtube.com/embed/5xl6LSxGy-Q?si=NCWdj7Zwy8JGVp-F",
+        link: "https://www.youtube.com/embed/5xl6LSxGy-Q?si=NCWdj7Zwy8JGVp-F&autoplay=1&loop=1",
     },
 ];
