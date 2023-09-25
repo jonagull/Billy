7dc73d835d3eb962354668f856ee23fd89ea8732
jonagull <jonathangullik@gmail.com>
2023-09-25 09:07:45 +0200

    updated FE deploy script

--- deployScripts/deployFE.sh
+++ deployScripts/deployFE.sh
@@ -1,2 +1,3 @@
-ssh root@boi "cd /root/projects/Billy/frontend; git fetch; git reset --hard master; npm i -f;  npm run build; pm2 reload 0; printf 'Deploy done'"
+#ssh root@boi "cd /root/projects/Billy/frontend; git fet# ch; git reset --hard master; npm i -f;  npm run build; pm2 reload 0; printf 'Deploy done'"
+ssh root@boi "cd /root/projects/Billy/frontend; git pull; npm i -f;  npm run build; pm2 reload 0; printf 'Deploy done'"
 
