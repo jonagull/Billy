d0a6c14be173c6c0390ba75c8493c1a7f937f228
jonagull <jonathangullik@gmail.com>
2023-11-08 17:36:00 +0100

    added combobox, changed layout of gamelogging

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/kit/vite";
+import { preprocessMeltUI } from "@melt-ui/pp";
+import sequence from "svelte-sequential-preprocessor";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
-    preprocess: [vitePreprocess({})],
+    preprocess: sequence([
+        // ... other preprocessors
+        vitePreprocess({}),
+        preprocessMeltUI(), // add to the end!
+    ]),
 
     kit: {
         prerender: {
